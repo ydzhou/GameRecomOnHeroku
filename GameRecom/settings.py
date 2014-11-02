d191d708aa09d0f80e3753a4670ce182a7e30c36
Yudi Zhou <yd.zhou@me.com>
2014-11-02 13:19:48 -0600

    bug fixed

--- GameRecom/settings.py
+++ GameRecom/settings.py
@@ -78,12 +78,6 @@ STATIC_URL = '/static/'
 import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-    }
-}
-
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
